f571e413ed3243bd889d80293e84c93e9469af88
duanqing.xing <duanqing.xing@brgroup.com>
2021-08-17 11:33:09 +0800

    server-feat: init

--- server/index.js
+++ server/index.js
@@ -3,25 +3,25 @@ const express = require('express')
 const app = express()
 
 // 全局秘钥
-app.set('secret', 'iihll')
+// app.set('secret', 'iihll')
 
 // 设置跨域
-app.use(require('cors')())
+// app.use(require('cors')())
 
 // 解析 json
 app.use(express.json())
 
-app.use('/uploads', express.static(__dirname + '/uploads'))
-app.use('/admin', express.static(__dirname + '/admin'))
-app.use('/', express.static(__dirname + '/web'))
+// app.use('/uploads', express.static(__dirname + '/uploads'))
+// app.use('/admin', express.static(__dirname + '/admin'))
+// app.use('/', express.static(__dirname + '/web'))
 
 // 引入数据库连接
-require('./plugins/db')(app)
+// require('./plugins/db')(app)
 
 // 引入 admin 路由
 require('./routes/admin')(app)
-require('./routes/web')(app)
+// require('./routes/web')(app)
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
+app.listen(9999, () => {
+  console.log('http://localhost:9999')
 })
