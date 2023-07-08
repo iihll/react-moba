71337f6951afa016635e3c820fca6d9587251091
iihll <iihll.dev@gmail.com>
2023-07-08 05:30:59 -0700

    feat: 服务端相关代码迁移

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -7,7 +7,7 @@ const schema = new mongoose.Schema({
     // 设置 false 数据查询时查不出
     select: false,
     set(val) {
-      return require('bcryptjs').hashSync(val)
+      return require('bcrypt').hashSync(val, 10)
     }
   }
 })
