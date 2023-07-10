1cd4235c9f9666234ac803fc0e387884fcaa24e7
iihll <iihll.dev@gmail.com>
2023-07-10 22:23:02 +0800

    feat: admin 分类列表弹窗

--- admin/src/pages/CategoryList/index.tsx
+++ admin/src/pages/CategoryList/index.tsx
@@ -1,8 +1,14 @@
-import React, { useRef } from 'react'
-import { PlusOutlined } from '@ant-design/icons'
+import React, { useRef, useState } from 'react'
 import type { ActionType, ProColumns } from '@ant-design/pro-components'
-import { ProTable } from '@ant-design/pro-components'
-import { Button } from 'antd'
+import {
+  ModalForm,
+  ProForm,
+  ProFormDateRangePicker,
+  ProFormSelect,
+  ProFormText,
+  ProTable,
+} from '@ant-design/pro-components'
+import { Button, Form, message } from 'antd'
 import http from '@/utils/http'
 
 interface ListItem {
@@ -34,23 +40,21 @@ const columns: ProColumns<ListItem>[] = [
     valueType: 'option',
     key: 'option',
     render: (text, record, _, action) => [
-      <a
-        key="editable"
-
-      >
+      <Button type="link" key="edit">
         编辑
-      </a>,
-      <a target="_blank" rel="noopener noreferrer" key="view">
-        查看
-      </a>,
+      </Button>,
     ],
   },
 ]
 
 export default function CategoryList() {
   const actionRef = useRef<ActionType>()
+  const [isAdd, setIsAdd] = useState(false)
+  const [form] = Form.useForm<{ name: string; company: string }>()
+
   return (
-    <ProTable<ListItem>
+    <>
+      <ProTable<ListItem>
       headerTitle="分类列表"
       columns={columns}
       actionRef={actionRef}
@@ -66,18 +70,92 @@ export default function CategoryList() {
       search={false}
       options={false}
       pagination={false}
-      toolBarRender={() => [
-        <Button
-          key="button"
-          icon={<PlusOutlined />}
-          onClick={() => {
-            actionRef.current?.reload()
-          }}
-          type="primary"
-        >
-          新建
-        </Button>,
-      ]}
+      toolBarRender={(action) => {
+        return [<Button key="create" type="primary">新建</Button>]
+      }}
     />
+    <ModalForm<{
+      name: string
+      company: string
+    }>
+      title={isAdd ? '新建' : '编辑'}
+      form={form}
+      autoFocusFirstInput
+      modalProps={{
+        destroyOnClose: true,
+        onCancel: () => console.log('run'),
+      }}
+      submitTimeout={2000}
+      onFinish={async (values) => {
+        console.log(values.name)
+        message.success('提交成功')
+        return true
+      }}
+    >
+      <ProForm.Group>
+        <ProFormText
+          width="md"
+          name="name"
+          label="签约客户名称"
+          tooltip="最长为 24 位"
+          placeholder="请输入名称"
+        />
+
+        <ProFormText
+          width="md"
+          name="company"
+          label="我方公司名称"
+          placeholder="请输入名称"
+        />
+      </ProForm.Group>
+      <ProForm.Group>
+        <ProFormText
+          width="md"
+          name="contract"
+          label="合同名称"
+          placeholder="请输入名称"
+        />
+        <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
+      </ProForm.Group>
+      <ProForm.Group>
+        <ProFormSelect
+          request={async () => [
+            {
+              value: 'chapter',
+              label: '盖章后生效',
+            },
+          ]}
+          width="xs"
+          name="useMode"
+          label="合同约定生效方式"
+        />
+        <ProFormSelect
+          width="xs"
+          options={[
+            {
+              value: 'time',
+              label: '履行完终止',
+            },
+          ]}
+          name="unusedMode"
+          label="合同约定失效效方式"
+        />
+      </ProForm.Group>
+      <ProFormText width="sm" name="id" label="主合同编号" />
+      <ProFormText
+        name="project"
+        disabled
+        label="项目名称"
+        initialValue="xxxx项目"
+      />
+      <ProFormText
+        width="xs"
+        name="mangerName"
+        disabled
+        label="商务经理"
+        initialValue="启途"
+      />
+    </ModalForm>
+    </>
   )
 }
